8da737cd0f4acb5c203af78c4ec95aa040ff27e3
Kipling2333 <kiplingbowier@gmail.com>
2026-06-12 22:31:18 +0200

    update swapped thumbnail

--- src/lib/seed.ts
+++ src/lib/seed.ts
@@ -10,7 +10,7 @@ export const SEED_MOVIES: Movie[] = [
     year: 2026,
     rating: 8.0,
     duration: 130,
-    thumbnailUrl: "https://image.mux.com/Y4ANziekvHvOMFkLwx161hGFhcCCx9EU5tvWhq1JsZw/thumbnail.jpg",
+    thumbnailUrl: "https://i.ibb.co/mrtkbX5c/swapped-movie-thumbnail.jpg",
     accent: "#e11d2e",
     videoUrl: "https://stream.mux.com/Y4ANziekvHvOMFkLwx161hGFhcCCx9EU5tvWhq1JsZw",
     trailerUrl: "https://www.youtube.com/embed/i-Dd_zjzRcs",
